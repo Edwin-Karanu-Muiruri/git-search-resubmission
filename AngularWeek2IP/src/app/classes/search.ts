d1e241ef4eedb1d4fd2a1aa105710ae425e329f8
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-12 21:26:35 +0300

    Added an about component for the about section.

--- AngularWeek2IP/src/app/classes/search.ts
+++ AngularWeek2IP/src/app/classes/search.ts
@@ -1,2 +1,9 @@
+import {User} from '../classes/user';
+import {Repository} from '../classes/repository';
+import {HttpClient} from '@angular/common/http';
+import { from } from 'rxjs';
 export class Search {
+    constructor(private user:any, private repository:any){
+
+    }
 }
--- AngularWeek2IP/src/app/github-http/github-request.service.ts
+++ AngularWeek2IP/src/app/github-http/github-request.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Search} from '../classes/search';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
+  search: Search;
+
+  constructor(private http:HttpClient) {
+    this.search = new Search('user','repository');
+   }
   
-  constructor() { }
+   searchRequest(){
+     interface ApiResponse{
+       user:any;
+       repository:any;
+     }
+     let promise = new Promise((resolve,reject)=>{
+       this.http.get<ApiResponse>(environment.accessKey).toPromise().then(response=>{
+         this.search.user = search.user
+         this.search.repository = search.repository
+
+         resolve()
+       },
+       error=>{
+         this.search.user = "Ooops, there doesn't seem to be a user with that name. Try Checking your spelling."
+         this.search.repository = "Ooops, there doesn't seem to be a repository with that name. Try Checking your spelling."
+
+         reject (error)
+       })
+     })
+     return promise
+   }
 }
