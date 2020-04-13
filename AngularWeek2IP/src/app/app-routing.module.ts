301130b7e6b776ad93e1abac7805adaa2727ff77
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-13 08:24:48 +0300

    Added a navbar for smooth movement between pages.

--- AngularWeek2IP/src/app/app-routing.module.ts
+++ AngularWeek2IP/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 
 const routes: Routes = [
-  {path: 'searchResults', component:},
+  {path: 'searchResults', component:SearchResultsComponent},
   {path: 'about', component: AboutComponent}
 
 ];
--- AngularWeek2IP/src/app/app.module.ts
+++ AngularWeek2IP/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{NgProgressModule} from '@ngx-progressbar/core';
+import {NgProgressHttpClientModule} from '@ngx-progressbar/http-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgProgressModule,
+    NgProgressHttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
